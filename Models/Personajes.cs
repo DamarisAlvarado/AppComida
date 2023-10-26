6b182af6b6f71a85eb77ab5be55e51e629ed71d3
Componentes <damarisarciga1520@gmail.com>
2023-10-26 17:23:13 -0600

    Personajes

---
+++ Models/Personajes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppComida.Models
+{
+    public class Personajes
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+    }
+}
