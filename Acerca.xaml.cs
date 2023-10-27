5234fd11f42772f75ac87f4c18910d07b38be2e1
Samantha <samcalderon2510@gmail.com>
2023-10-26 20:31:18 -0600

    23

--- Acerca.xaml.cs
+++ Acerca.xaml.cs
@@ -2,11 +2,10 @@ namespace AppComida;
 
 public partial class Acerca : ContentPage
 {
-    private readonly ILauncher laucher;
 
-    public Acerca(ILauncher laucher)
+    public Acerca()
 	{
 		InitializeComponent();
-        this.laucher = laucher;
+    
     }
 }
