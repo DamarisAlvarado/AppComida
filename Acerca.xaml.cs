2f8b9121d59386d612d928962fae7b4def8c5044
Samantha <samcalderon2510@gmail.com>
2023-10-26 20:51:02 -0600

    prueba 2

--- Acerca.xaml.cs
+++ Acerca.xaml.cs
@@ -2,18 +2,10 @@ namespace AppComida;
 
 public partial class Acerca : ContentPage
 {
-    private readonly ILauncher launcher;
 
-    public Acerca(ILauncher launcher)
+    public Acerca()
 	{
 		InitializeComponent();
-        this.launcher = launcher;
-    }
-    private async void btnwhatsapp(object sender, EventArgs e)
-    {
-        var Number = 811659756;
-        var url = $"https://wa.me/{Number}?text=Hola mundo";
-        await launcher.OpenAsync(url);
-
+       
     }
 }
