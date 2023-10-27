72984d88a35196cd5934f5e41220692770eaa6f6
Amel Delgado <137000297+AmelCG@users.noreply.github.com>
2023-10-26 21:47:17 -0600

    Diseño de menu

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,7 +7,7 @@
             InitializeComponent();
 
             NavigationPage navigationPage = new NavigationPage(new MainPage(new ViewModels.LoginViewModel()));
-            Color color = Color.FromArgb("#24325F");
+            Color color = Color.FromArgb("#526E2D");
             navigationPage.BarBackgroundColor = color;
             navigationPage.BarTextColor = new Color(255, 255, 255);
 
