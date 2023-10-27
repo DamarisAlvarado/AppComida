b6c5046815daeaae63e895d2026be56fbbe1ddb9
Amel Delgado <137000297+AmelCG@users.noreply.github.com>
2023-10-26 21:31:58 -0600

    Cambio de colores

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,7 +7,7 @@
             InitializeComponent();
 
             NavigationPage navigationPage = new NavigationPage(new MainPage(new ViewModels.LoginViewModel()));
-            Color color = Color.FromArgb("#0247FE");
+            Color color = Color.FromArgb("#24325F");
             navigationPage.BarBackgroundColor = color;
             navigationPage.BarTextColor = new Color(255, 255, 255);
 
